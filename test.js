75e3ce2cbc54ca3c8042c85fc2f1f10a1005e628
Simple <80062103+zatsjyouji@users.noreply.github.com>
2021-10-09 13:50:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 document.getElementById("login").onclick = function(){
-    location.href = "https://sites.google.com/view/simpleworld/%E3%83%9B%E3%83%BC%E3%83%A0"
+    location.href = "https://zatsjyouji.github.io/youtubesample2"
 }
