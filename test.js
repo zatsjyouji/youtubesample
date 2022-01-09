7707160e222aad95c9c3ee92e816ba8fe555c6e5
Simple <80062103+zatsjyouji@users.noreply.github.com>
2022-01-09 22:59:05 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,5 @@ document.getElementById("login").onclick = function(){
     location.href = "https://sites.google.com/view/simpleworld/%E3%83%9B%E3%83%BC%E3%83%A0"
 }
 document.getElementById("situmonyou").onclick = function(){
-    testwindow = window.open("","samplewindow",width="300",height="300");
-    testwindow.document.write("oraora")
-
+    testwindow = window.open("https://zatsjyouji.github.io/simplepopuptest/","samplewindow",width="300",height="300");
 }
