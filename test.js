f492f8f8bed6945a8f23a73fb8ca84b041b55e99
Simple <80062103+zatsjyouji@users.noreply.github.com>
2022-01-09 22:25:51 +0900

    click add

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 document.getElementById("login").onclick = function(){
-    location.href = "https://zatsjyouji.github.io/youtubesample2"
+    location.href = "https://sites.google.com/view/simpleworld/%E3%83%9B%E3%83%BC%E3%83%A0"
+}
+document.getElementById("situmonyou").onclick = function(){
+    testwindow = window.open("","samplewindow",width="300",height="300");
+    testwindow.document.write("oraora")
+
 }
